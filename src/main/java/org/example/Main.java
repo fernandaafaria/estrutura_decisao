4fe7a10f04e4fcb8b91c8cc00523ef2bdfddce1f
FERNANDA FARIA DA SILVA <fernanda.silva124@senaisp.edu.br>
2026-09-01 13:46:08 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.Scanner;
+
+// Questão 1 - Verificar Maioridade
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int idade;
+        System.out.println("Digite sua idade:");
+        idade = entrada.nextInt();
+
+        if (idade >=18 ){
+            System.out.println("Maior de idade");
+        } else {
+            System.out.println("Menor de idade");
+        }
+    }
+}
